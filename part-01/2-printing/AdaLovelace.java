57f9b5ae94e83fb42e1357e3764519e97ea9976f
Ryner <individualog@gmail.com>
2026-06-10 19:08:35 +0100

    Fix Urheilutilastot filename and invalid class name

--- part-01/2-printing/AdaLovelace.java
+++ part-01/2-printing/AdaLovelace.java
@@ -15,6 +15,7 @@
 public class AdaLovelace {
     public static void main(String[] args) {
         // Write your program here
-
+      
+      System.out.println("Hello World, I am Ryner");
     }
 }
