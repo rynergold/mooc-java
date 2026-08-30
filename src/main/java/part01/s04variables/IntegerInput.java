f73c1f829baa70e2a59ccd1809751993af1e893d
Ryner <rynergold@users.noreply.github.com>
2026-08-30 16:42:41 +0100

    feat(class): completed the integer input exercise to learn how to take an integer input from the user and concatenate with a string

--- src/main/java/part01/s04variables/IntegerInput.java
+++ src/main/java/part01/s04variables/IntegerInput.java
@@ -3,9 +3,12 @@ package part01.s04variables;
 import java.util.Scanner;
 
 public class IntegerInput {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-    }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give a number:");
+    int n = Integer.valueOf(scanner.nextInt());
+    System.out.println("You gave the number " + n);
+  }
 }
