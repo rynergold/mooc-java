89b7ac69b6297c87a456ddb42425d130e26c7d0b
Ryner <rynergold@users.noreply.github.com>
2026-09-01 01:38:18 +0100

    feat: finished sumoftwonums exercise

--- src/main/java/part01/s05calculating/SumOfTwoNumbers.java
+++ src/main/java/part01/s05calculating/SumOfTwoNumbers.java
@@ -3,10 +3,15 @@ package part01.s05calculating;
 import java.util.Scanner;
 
 public class SumOfTwoNumbers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
-    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give the first number:");
+    int f = Integer.valueOf(sc.nextLine());
+    System.out.println("Give the second number:");
+    int n = Integer.valueOf(sc.nextLine());
+    int s = f + n;
+    System.out.println("The sum of the numbers is " + s);
+  }
 }
