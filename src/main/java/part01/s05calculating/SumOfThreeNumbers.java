9a1f12c159580150f4a2ecfaa7746ac84e59642b
Ryner <rynergold@users.noreply.github.com>
2026-09-01 01:48:38 +0100

    feat: completed sumofthreenums exercise

--- src/main/java/part01/s05calculating/SumOfThreeNumbers.java
+++ src/main/java/part01/s05calculating/SumOfThreeNumbers.java
@@ -3,10 +3,17 @@ package part01.s05calculating;
 import java.util.Scanner;
 
 public class SumOfThreeNumbers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
-    }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give the first number:");
+    int first = Integer.valueOf(scanner.nextLine());
+    System.out.println("Give the second number:");
+    int sec = Integer.valueOf(scanner.nextLine());
+    System.out.println("Give the third number:");
+    int third = Integer.valueOf(scanner.nextLine());
+    int sum = first + sec + third;
+    System.out.println("The sum of the numbers is " + sum);
+  }
 }
