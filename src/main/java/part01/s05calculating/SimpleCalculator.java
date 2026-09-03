0b670e9062ea1d69a0b7e57d16e2b26eabe9c20e
Ryner <rynergold@users.noreply.github.com>
2026-09-03 18:28:57 +0100

    feat: complete challenge exercise simplecalculator

--- src/main/java/part01/s05calculating/SimpleCalculator.java
+++ src/main/java/part01/s05calculating/SimpleCalculator.java
@@ -3,10 +3,21 @@ package part01.s05calculating;
 import java.util.Scanner;
 
 public class SimpleCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
-    }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give the first number:");
+    int first = Integer.valueOf(scanner.nextLine());
+    System.out.println("Give the second number:");
+    int second = Integer.valueOf(scanner.nextLine());
+    System.out.println(first + " + " + second + " = " + (first + second));
+    System.out.println(first + " - " + second + " = " + (first - second)
+    );
+    System.out.println(first + " * " + second + " = " + (first * second)
+    );
+    System.out.println(first + " / " + second + " = " + ((double) first / second)
+    );
+    
+  }
 }
