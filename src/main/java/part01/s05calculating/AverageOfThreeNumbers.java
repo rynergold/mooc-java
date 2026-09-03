43bd6f0430c88f71dc6b6ab1fbd7848f819446e8
Ryner <rynergold@users.noreply.github.com>
2026-09-03 18:28:46 +0100

    feat: complete averageofthreenumbers exercise

--- src/main/java/part01/s05calculating/AverageOfThreeNumbers.java
+++ src/main/java/part01/s05calculating/AverageOfThreeNumbers.java
@@ -3,10 +3,18 @@ package part01.s05calculating;
 import java.util.Scanner;
 
 public class AverageOfThreeNumbers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
-    }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give the first number:");
+    double first = Double.valueOf(scanner.nextLine());
+    System.out.println("Give the second number:");
+    double second = Double.valueOf(scanner.nextLine());
+    System.out.println("Give the third number:");
+    double third = Double.valueOf(scanner.nextLine());
+    double avg = ((first + second + third) * 1.0) / 3;
+    System.out.println("The average is " + avg);
+    
+  }
 }
