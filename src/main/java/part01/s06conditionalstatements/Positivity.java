9298d3689e69880b4a4a19109e876b9251ed38aa
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:35:46 +0100

    feat: complete positivity exercise

--- src/main/java/part01/s06conditionalstatements/Positivity.java
+++ src/main/java/part01/s06conditionalstatements/Positivity.java
@@ -3,10 +3,16 @@ package part01.s06conditionalstatements;
 import java.util.Scanner;
 
 public class Positivity {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+  
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give a number:");
+    int num = Integer.parseInt(s.nextLine());
+    if (num > 0) {
+      System.out.println("The number is positive.");
     }
+    System.out.println("The number is not positive.");
+  }
 }
