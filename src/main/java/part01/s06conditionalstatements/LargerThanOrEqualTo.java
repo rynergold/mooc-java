c0fe87973a9e3d3a5325c3c89543186291a9f5e2
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:47:02 +0100

    feat: complete largerthanorequalto exercise

--- src/main/java/part01/s06conditionalstatements/LargerThanOrEqualTo.java
+++ src/main/java/part01/s06conditionalstatements/LargerThanOrEqualTo.java
@@ -3,22 +3,22 @@ package part01.s06conditionalstatements;
 import java.util.Scanner;
 
 public class LargerThanOrEqualTo {
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        // Write your program here
-      System.out.println("Give the first number:");
-      int first = Integer.valueOf(s.nextLine());
-      System.out.println("Give the second number:");
-      int second = Integer.valueOf(s.nextLine());
-      
-      if(first > second){
-        System.out.println("Greater number is: " + first);
-      } else if (second > first ){
-        System.out.println("Greater number is: " + second);
-      } else {
-        System.out.println("The numbers are equal!");
-      }
+  
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give the first number:");
+    int first = Integer.valueOf(s.nextLine());
+    System.out.println("Give the second number:");
+    int second = Integer.valueOf(s.nextLine());
+    
+    if (first > second) {
+      System.out.println("Greater number is: " + first);
+    } else if (second > first) {
+      System.out.println("Greater number is: " + second);
+    } else {
+      System.out.println("The numbers are equal!");
     }
+  }
 }
