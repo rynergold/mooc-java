bf97ce9d2839e422f47eaa0d5933cab1dd46ed76
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:52:06 +0100

    feat: complete OddOrEven exercise

--- src/main/java/part01/s06conditionalstatements/OddOrEven.java
+++ src/main/java/part01/s06conditionalstatements/OddOrEven.java
@@ -3,10 +3,17 @@ package part01.s06conditionalstatements;
 import java.util.Scanner;
 
 public class OddOrEven {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+  
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give a number:");
+    int number = Integer.valueOf(s.nextLine());
+    boolean isEven = number % 2 == 0;
+    if (isEven) {
+      System.out.println("Number " + number + " is even.");
     }
+    System.out.println("Number " + number + " is odd.");
+  }
 }
