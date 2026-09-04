ddf019ccf405188a306a22b61649197220491da0
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:41:01 +0100

    feat: complete largerthanorequalto class

--- src/main/java/part01/s06conditionalstatements/LargerThanOrEqualTo.java
+++ src/main/java/part01/s06conditionalstatements/LargerThanOrEqualTo.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         // Write your program here
+      System.out.println("Give the first number:");
+      int first = Integer.valueOf(s.nextLine());
+      System.out.println("Give the second number:");
+      int second = Integer.valueOf(s.nextLine());
+      
+      if(first > second){
+        System.out.println("Greater number is: " + first);
+      } else if (second > first ){
+        System.out.println("Greater number is: " + second);
+      } else {
+        System.out.println("The numbers are equal!");
+      }
     }
 }
