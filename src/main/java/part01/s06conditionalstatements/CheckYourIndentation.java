fe37bd863e8a90ffbf991d20d82f2fb1ab7f2a7a
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:31:02 +0100

    feat: completed checkyourindentation exercise

--- src/main/java/part01/s06conditionalstatements/CheckYourIndentation.java
+++ src/main/java/part01/s06conditionalstatements/CheckYourIndentation.java
@@ -3,22 +3,22 @@ package part01.s06conditionalstatements;
 import java.util.Scanner;
 
 public class CheckYourIndentation {
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-System.out.println("Give the first number:");
-int first = Integer.valueOf(scan.nextLine());
-System.out.println("Give the second number:");
-int second = Integer.valueOf(scan.nextLine());
-
-if (first == second) {
-System.out.println("Same!");
- } else if (first > second) {
-    System.out.println("The first was larger than the second!");
-} else {
-       System.out.println("The second was larger than the first!");
-}
-
+  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Give the first number:");
+    int first = Integer.valueOf(scan.nextLine());
+    System.out.println("Give the second number:");
+    int second = Integer.valueOf(scan.nextLine());
+    
+    if (first == second) {
+      System.out.println("Same!");
+    } else if (first > second) {
+      System.out.println("The first was larger than the second!");
+    } else {
+      System.out.println("The second was larger than the first!");
     }
+    
+  }
 }
