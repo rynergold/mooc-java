7ac23773e153fe91611bc3aa1b8a7f281a91ab27
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:58:48 +0100

    feat: complete Same exercise
    Further practice with .equals() and conditional statements to check if
    two strings are equal.

--- src/main/java/part01/s06conditionalstatements/Same.java
+++ src/main/java/part01/s06conditionalstatements/Same.java
@@ -8,5 +8,15 @@ public class Same {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+      
+      System.out.println("Enter the first string:");
+      String string1 = scanner.nextLine();
+      System.out.println("Enter the second string:");
+      String string2 = scanner.nextLine();
+      boolean areStringsEqual = string1.equals(string2);
+      if (areStringsEqual) {
+        System.out.println("Same");
+      }
+      System.out.println("Different");
     }
 }
