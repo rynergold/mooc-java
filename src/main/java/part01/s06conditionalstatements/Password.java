3f03aca9340a280ceb469ff2701df4d02e30c666
Ryner <rynergold@users.noreply.github.com>
2026-09-04 20:56:01 +0100

    feat: complete Password exercise
    Here I learned that using == between Strings does not compare the
    sequence of characters but the address registered on the "Heap". When I
    create a variable of type String, it creates an object and using ==
    compares the address not the characters so If I want to compare the two
    then I need to use .equal()

--- src/main/java/part01/s06conditionalstatements/Password.java
+++ src/main/java/part01/s06conditionalstatements/Password.java
@@ -3,10 +3,16 @@ package part01.s06conditionalstatements;
 import java.util.Scanner;
 
 public class Password {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+  
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Password?");
+    String pw = s.nextLine();
+    if (pw.equals("Caput Draconis")) {
+      System.out.println("Welcome!");
     }
+    System.out.println("Off with you!");
+  }
 }
