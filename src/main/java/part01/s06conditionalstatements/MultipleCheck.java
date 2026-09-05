e60d3de55410f175787d6a38cdb1cc325f1b5900
Ryner <rynergold@users.noreply.github.com>
2026-09-05 13:14:52 +0100

    feat: create new exercises for section 6.5

---
+++ src/main/java/part01/s06conditionalstatements/MultipleCheck.java
@@ -0,0 +1,12 @@
+package part01.s06conditionalstatements;
+
+import java.util.Scanner;
+
+public class MultipleCheck {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+    }
+}
