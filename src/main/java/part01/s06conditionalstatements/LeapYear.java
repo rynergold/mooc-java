98ef1abc1d98c1780fddd6e1cad80568b6b9f3e4
Ryner <rynergold@users.noreply.github.com>
2026-09-05 10:28:46 +0100

    feat: completed LeapYear exercise
    In this exercise, I learned to apply conditional statmeents and logical
    operators on a higher difficulty than the previous questions. Tested my
    logic skills.

--- src/main/java/part01/s06conditionalstatements/LeapYear.java
+++ src/main/java/part01/s06conditionalstatements/LeapYear.java
@@ -3,10 +3,24 @@ package part01.s06conditionalstatements;
 import java.util.Scanner;
 
 public class LeapYear {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Give a year:");
+    int year = Integer.parseInt(scanner.nextLine());
+    
+    boolean isDivisibleBy4 = year % 4 == 0;
+    boolean isDivisibleBy100 = year % 100 == 0;
+    boolean isDivisibleBy400 = year % 400 == 0;
+    
+    if (!isDivisibleBy100 && isDivisibleBy4) {
+      System.out.println("The year is a leap year.");
+    } else if (isDivisibleBy100 && isDivisibleBy400) {
+      System.out.println("The year is a leap year.");
+    } else {
+      System.out.println("The year is not a leap year.");
     }
+  }
 }
