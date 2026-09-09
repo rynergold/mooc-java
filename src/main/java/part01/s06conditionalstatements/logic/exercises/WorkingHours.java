c1175c60ab651625b3348bf22f963f1097104947
Ryner <rynergold@users.noreply.github.com>
2026-09-09 15:52:50 +0200

    feat: complete workinghours exercise

--- src/main/java/part01/s06conditionalstatements/logic/exercises/WorkingHours.java
+++ src/main/java/part01/s06conditionalstatements/logic/exercises/WorkingHours.java
@@ -3,10 +3,17 @@ package part01.s06conditionalstatements.logic.exercises;
 import java.util.Scanner;
 
 public class WorkingHours {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+  
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    // Write your program here
+    System.out.println("Enter hour (0-23):");
+    int time = Integer.parseInt(s.nextLine());
+    if (time < 9 || time > 17) {
+      System.out.println("Office closed");
+      return;
     }
+    System.out.println("Office open");
+  }
 }
