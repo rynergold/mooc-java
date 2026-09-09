7f95cb224a1616584c5b104df61603718f63f709
Ryner <rynergold@users.noreply.github.com>
2026-09-09 15:58:33 +0200

    feat: complete multiplecheck exercise

--- src/main/java/part01/s06conditionalstatements/logic/exercises/MultipleCheck.java
+++ src/main/java/part01/s06conditionalstatements/logic/exercises/MultipleCheck.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class MultipleCheck {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         // Write your program here
+      System.out.println("Enter first number:");
+      int a = Integer.parseInt(s.nextLine());
+      System.out.println("Enter second number:");
+      int b = Integer.parseInt(s.nextLine());
+      if (a % b == 0 || b % a == 0){
+        System.out.println("Multiples");
+        return;
+      }
+      System.out.println("Not multiples");
     }
 }
