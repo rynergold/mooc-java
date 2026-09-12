3407ba82c4d7f60b398a3bd46fe8c0a380fe2fdd
Ryner <rynergold@users.noreply.github.com>
2026-09-12 20:57:37 +0200

    feat: complete ticket price exercise

--- src/main/java/part01/s06conditionalstatements/logic/exercises/TicketPrice.java
+++ src/main/java/part01/s06conditionalstatements/logic/exercises/TicketPrice.java
@@ -3,10 +3,20 @@ package part01.s06conditionalstatements.logic.exercises;
 import java.util.Scanner;
 
 public class TicketPrice {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // Write your program here
+    String prompt = "Enter age:";
+    String fiveEuros = "Ticket price: 5 euros";
+    String tenEuros = "Ticket price: 10 euros";
+    System.out.println(prompt);
+    int age = Integer.parseInt(sc.nextLine());
+    boolean isAgeLessThan12 = age < 12;
+    if (isAgeLessThan12) {
+      System.out.println(fiveEuros);
+      return;
     }
+    System.out.println(tenEuros);
+  }
 }
